350514bfd88e6bf8583b350555051f9a674969b7
Martin-EvilDeath <emilexjun238@gmail.com>
2023-09-14 17:05:41 -0600

    mas practicas
    OwO

--- Unidad 2/AP/PET.ino
+++ Unidad 2/AP/PET.ino
@@ -1,5 +1,6 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <ArduinoJson.h>
 
 const char *ssid = "BLACKPINK";
 const char *password = "itcolima6";
@@ -14,6 +15,49 @@ unsigned long lastTime = 0;
 // unsigned long timerDelay = 600000;
 // Set timer to 5 seconds (5000)
 unsigned long timerDelay = 5000;
+
+void post_data(String action, int quantity)
+{
+  // Objeto JSON
+  DynamicJsonDocument json_chido(1024);
+  json_chido["action"] = action;
+  json_chido["quantity"] = quantity;
+
+  // Cadena JSON para enviar
+  String json_str;
+  serializeJson(json_chido, json_str);
+
+  // Enviar POST
+  HTTPClient http;
+  http.begin(serverName);
+  http.addHeader("Content-Type", "application/json");
+  int httpResponseCode = http.POST(json_str);
+
+  if (httpResponseCode > 0)
+  {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    String payload = http.getString();
+    Serial.println(payload);
+  }
+  else
+  {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+
+  http.end();
+}
+
+void post_asc()
+{
+  post_data("asc", 1);
+}
+
+void post_desc()
+{
+  post_data("desc", 1);
+}
 
 void setup()
 {
@@ -35,6 +79,22 @@ void setup()
 
 void loop()
 {
+  if (Serial.available())
+  {
+    String data = Serial.readStringUntil('\n');
+    if (data == "+")
+    {
+      post_asc();
+    }
+    else if (data == "-")
+    {
+      post_desc();
+    }
+    else
+    {
+      Serial.println("Invalid command");
+    }
+  }
   // Send an HTTP POST request every 10 minutes
   if ((millis() - lastTime) > timerDelay)
   {
